2b351e2cea47b2025ea0749fdc505013696a49b2
Shoriful <shorifulbba0360@gmail.com>
2025-10-31 21:17:19 +0600

    get data from mongodb and sort data by id

--- src/app/page/home/component/ProductsSection.jsx
+++ src/app/page/home/component/ProductsSection.jsx
@@ -8,17 +8,21 @@ import ProductCard from "./Card";
 export default async function ProductsSection() {
 
   const productsCollection=dbConnect(collectionNameObj.productCollection)
-  const data=await productsCollection.find().toArray()
+  const data=await productsCollection.find()
+  .sort({_id:-1})
+  .limit(5)
+  .toArray()
 
 const products=data.map(item=>({
   ...item,
-  _id:item._id.toString()
+   id: item._id.toString(),
+  _id: undefined
 }))
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
       {
-        products?.map((detail)=><ProductCard key={detail._id}
+        products?.map((detail)=><ProductCard key={detail.id}
    detail={detail}
       />)
       }
